dfa3c09d1f25116ccc3fa828697b4223b7d4748f
John Strand <john.strand@dekiru.se>
2020-07-24 16:47:22 +0200

    Utility methods for identifying tokens

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,11 @@
+import {
+  Token,
+  AnyValueToken,
+  TokenType,
+  IdentifierToken,
+  SymbolToken,
+} from "./Tokenizer";
+
 export const Char = {
   isWhitespace(str: string) {
     return str.trim() === "";
@@ -9,6 +17,24 @@ export const Char = {
 
   isComment(str: string) {
     return str === "#";
+  },
+};
+
+export const Tokens = {
+  isValueToken(token: Token): token is AnyValueToken {
+    return (
+      token.type === TokenType.Boolean ||
+      token.type === TokenType.Number ||
+      token.type === TokenType.String
+    );
+  },
+
+  isIdentifier(token: any): token is IdentifierToken {
+    return token && token.type == TokenType.Identifier;
+  },
+
+  isSymbol(token: any): token is SymbolToken {
+    return token && token.type == TokenType.Symbol;
   },
 };
 
